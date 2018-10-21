896a554149f68d5e5b97ac8ac656a65b0f28a92c
JorgeAndd <jorgeluizandrade@outlook.com>
2018-10-21 19:02:01 -0300

    Mobile sidedrawer

---
+++ BurgerApp/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+
+import classes from './SideDrawer.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems'
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer];
+    if(props.open) {
+        attachedClasses.push(classes.Open);
+    } else {
+        attachedClasses.push(classes.Close)
+    }
+
+    return (
+        <Fragment>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Fragment>
+    );
+};
+
+export default sideDrawer;
