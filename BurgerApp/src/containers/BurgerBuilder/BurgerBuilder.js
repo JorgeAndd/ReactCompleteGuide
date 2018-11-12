1cdfe3cd8d4a56ff45434998bad5a5bbef59bbce
Jorge Luiz <jorgeluizandrade@outlook.com>
2018-11-12 12:36:59 -0200

    Posting burger order to firebase

--- BurgerApp/src/containers/BurgerBuilder/BurgerBuilder.js
+++ BurgerApp/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+import { timingSafeEqual } from 'crypto';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -87,7 +89,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue...');
+        // alert('You continue...');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice, // Should recalculate on server
+            customer: {
+                name: 'Andd',
+                address: {
+                    street: 'Testing',
+                    zipCode: '12345',
+                    country: 'Brazil'
+                },
+                email: 'test@test.com',
+                deliveryMethod: 'fastest'
+            }
+        }        
+
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
