1c73249f3bf0593e663d8b9f2aeb9a2a7b03205f
Jorge Luiz <jorgeluizandrade@outlook.com>
2018-11-12 15:31:35 -0200

    Error handling high order component

--- BurgerApp/src/containers/BurgerBuilder/BurgerBuilder.js
+++ BurgerApp/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../components/withErrorHandler/withErrorHandler';
 
 import axios from '../../axios-orders';
 
@@ -109,13 +110,13 @@ class BurgerBuilder extends Component {
             }
         }
 
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({ loading: false, purchasing: false })
-        //     })
-        //     .catch(error => {
-        //         this.setState({ loading: false, purchasing: false })
-        //     });
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({ loading: false, purchasing: false })
+            })
+            .catch(error => {
+                this.setState({ loading: false, purchasing: false })
+            });
     }
 
     render() {
@@ -155,4 +156,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
