17d2c3a047d52e126adf0d700bf7b07d746e9c40
Jorge Luiz <jorgeluizandrade@outlook.com>
2018-12-07 11:48:39 -0200

    Guarding newpost route

--- Http/src/containers/Blog/Blog.js
+++ Http/src/containers/Blog/Blog.js
@@ -7,6 +7,10 @@ import NewPost from './NewPost/NewPost';
 import "./Blog.css";
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
+
     render() {
         return (
             <div className="Blog">
@@ -19,9 +23,9 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/newpost" component={NewPost} />
+                    {this.state.auth ? <Route path="/newpost" component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
-                    
+
                     <Redirect from="/" to="/posts" />
                 </Switch>
             </div>
--- Http/src/containers/Blog/NewPost/NewPost.js
+++ Http/src/containers/Blog/NewPost/NewPost.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Redirect } from 'react-router-dom';
 
 import "./NewPost.css";
 
@@ -21,7 +20,7 @@ class NewPost extends Component {
 
         axios.post("/posts", data)
             .then(response => {
-                console.log(response);               
+                console.log(response);              
 
                 this.props.history.push('/posts');
             });
