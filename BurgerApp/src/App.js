f8ef9ba4bf0bc834ce86bd98bffe766ae3e2b0cb
Jorge Luiz <jorgeluizandrade@outlook.com>
2018-12-11 14:12:37 -0200

    Checkout page and routing

--- BurgerApp/src/App.js
+++ BurgerApp/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <BrowserRouter>
                 <Layout>
-                    <BurgerBuilder />
+                    <Route path='/' exact component={BurgerBuilder} />
+                    <Route path='/checkout' component={Checkout} />
                 </Layout>
-            </div>
+            </BrowserRouter>
         );
     }
 }
 
 export default App;
+
