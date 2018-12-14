6fbfb85d53161b56d29cc1de85718d5f3da8336d
Jorge Luiz <jorgeluizandrade@outlook.com>
2018-12-14 11:43:57 -0200

    Created select input element

--- BurgerApp/src/components/UI/Input/Input.js
+++ BurgerApp/src/components/UI/Input/Input.js
@@ -7,13 +7,38 @@ const input = (props) => {
 
     switch(props.elementType) {
         case 'input':
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} />;
+            inputElement = <input 
+                className={classes.InputElement} 
+                value={props.value}                 
+                {...props.elementConfig} />;
             break;
         case 'textarea':
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} />;
+            inputElement = <textarea
+                className={classes.InputElement} 
+                value={props.value} 
+                {...props.elementConfig} />;
+            break;
+        case 'select':
+            inputElement = (
+                <select
+                    className={classes.InputElement} 
+                    value={props.value}>
+                    {props.elementConfig.options.map(op => (
+                        <option 
+                            value={op.value}
+                            key={op.value}>
+                                {op.displayValue}
+                        </option>
+                    ))}
+                    <option ></option>
+                </select>
+            );
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} />;
+            inputElement = <input 
+                className={classes.InputElement} 
+                value={props.value} 
+                {...props.elementConfig} />;
     }
 
     return (
